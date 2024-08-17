086cfe73992493715511ce0b1bcaa83f266ad938
Yoga Pangestu <yogapangestu16@gmail.com>
2024-08-17 13:56:26 +0700

    first commit

---
+++ vacancyORcareer-page/vacancy/assets/part/abias-home/main.js
@@ -0,0 +1,15 @@
+
+/*=====| 8. Button Hover Effect |=====*/
+  $(".dark-btn")
+  .on("mouseenter", function (e) {
+    var parentOffset = $(this).offset(),
+      relX = e.pageX - parentOffset.left,
+      relY = e.pageY - parentOffset.top;
+    $(this).find("span").css({ top: relY, left: relX });
+  })
+  .on("mouseout", function (e) {
+    var parentOffset = $(this).offset(),
+      relX = e.pageX - parentOffset.left,
+      relY = e.pageY - parentOffset.top;
+    $(this).find("span").css({ top: relY, left: relX });
+  });
